22a67d136ae6130e52305f8782bcbb147a30392c
jazyz <johnlikeapple37@gmail.com>
2022-01-17 20:12:31 -0500

    add comments

--- GameFrame.java
+++ GameFrame.java
@@ -1,25 +1,20 @@
+// This class is the frame for the main game
 
-/* GameFrame class establishes the frame (window) for the game
-It is a child of JFrame because JFrame manages frames
-Runs the constructor in GamePanel class
-
-*/
 import java.awt.*;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
 
-	public GameFrame() {
-		GamePanel gamePanel = new GamePanel(false, false);
+	public GameFrame(boolean p, boolean d) {
+		GamePanel gamePanel = new GamePanel(p, d);
 		this.add(gamePanel);
-		this.setTitle("Pong"); // set title for frame
-		this.setResizable(false); // frame can't change size
+		this.setTitle("Pong"); 
+		this.setResizable(false); 
 		this.setBackground(Color.white);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // X button will stop program execution
-		this.pack();// makes components fit in window - don't need to set JFrame size, as it will
-					// adjust accordingly
-		this.setVisible(true); // makes window visible to user
-		this.setLocationRelativeTo(null);// set window in middle of screen
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 		
 	}
 
--- MenuFrame.java
+++ MenuFrame.java
@@ -1,3 +1,5 @@
+// This class is the frame for the menu panel
+
 import java.awt.Color;
 import java.awt.event.ActionListener;
 
@@ -8,14 +10,13 @@ public class MenuFrame extends JFrame {
 	public MenuFrame() {
 		MenuPanel menu = new MenuPanel();
 		this.add(menu);
-		this.setTitle("Main Menu"); // set title for frame
-		this.setResizable(false); // frame can't change size
+		this.setTitle("Main Menu"); 
+		this.setResizable(false);
 		this.setBackground(Color.white);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // X button will stop program execution
-		this.pack();// makes components fit in window - don't need to set JFrame size, as it will
-					// adjust accordingly
-		this.setLocationRelativeTo(null);// set window in middle of screen
-		this.setVisible(true); // makes window visible to user
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
 	}
 
 }
